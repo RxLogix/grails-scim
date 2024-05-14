562a222f05aab742754cf0ad402a563fc83f9c2a
Nikhil <Nikhil.Kanyal@rxlogix.com>
2024-05-14 15:56:32 +0530

    updated grails version number

--- src/main/groovy/grails/plugins/scim/GrailsScimGrailsPlugin.groovy
+++ src/main/groovy/grails/plugins/scim/GrailsScimGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import grails.plugins.scim.binding.JsonScimApiDataBindingSourceCreator
 class GrailsScimGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.9 > *"
+    def grailsVersion = "6.2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
