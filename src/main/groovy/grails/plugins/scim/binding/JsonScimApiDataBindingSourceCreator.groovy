1fadc0aca961133144b179fdc61a9836f4be4e63
FarhanAli <farhan.ali@rxlogix.com>
2025-09-12 01:11:19 +0530

    PVS-95014: Mime type fix

--- src/main/groovy/grails/plugins/scim/binding/JsonScimApiDataBindingSourceCreator.groovy
+++ src/main/groovy/grails/plugins/scim/binding/JsonScimApiDataBindingSourceCreator.groovy
@@ -1,5 +1,6 @@
 package grails.plugins.scim.binding
 
+import grails.core.GrailsApplication
 import grails.web.mime.MimeType
 import groovy.transform.CompileStatic
 import org.grails.web.databinding.bindingsource.JsonApiDataBindingSourceCreator
@@ -7,11 +8,24 @@ import org.grails.web.databinding.bindingsource.JsonApiDataBindingSourceCreator
 @CompileStatic
 class JsonScimApiDataBindingSourceCreator extends JsonApiDataBindingSourceCreator {
 
+    GrailsApplication grailsApplication
+
     private static final MimeType SCIM_JSON = new MimeType('application/scim+json', 'json')
     private static final MimeType SCIM_JSON_UTF8 = new MimeType('application/scim+json;charset=utf-8', 'json')
 
     @Override
     MimeType[] getMimeTypes() {
+        List<String> configuredMimeTypes = (List<String>) grailsApplication.config.getProperty(
+                "grails.scim.mime.types", List
+        )
+
+        if (configuredMimeTypes != null && !configuredMimeTypes.isEmpty()) {
+            List<MimeType> mimeTypes = []
+            for (String mime : configuredMimeTypes) {
+                mimeTypes << new MimeType(mime, "json")
+            }
+            return mimeTypes as MimeType[]
+        }
         return [SCIM_JSON, SCIM_JSON_UTF8] as MimeType[]
     }
 }
